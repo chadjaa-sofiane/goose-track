59165cf08ee5135c84c30bfc882e5c3c34c73a62
chadjaa-sofiane <sofianne012@gmail.com>
2023-10-31 12:57:07 +0100

    add some theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
     content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                accents: {
+                    1: 'hsl(var(--color-accent-100) / <alpha-value>)',
+                    2: 'hsl(var(--color-accent-200) / <alpha-value>)',
+                    3: 'hsl(var(--color-accent-2) / <alpha-value>)',
+                },
+                bg: 'hsl(var(--color-bg) / <alpha-value>)',
+                text: 'hsl(var(--color-text) / <alpha-value>)',
+            },
+            fontFamily: {
+                Inter: ['Inter', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
