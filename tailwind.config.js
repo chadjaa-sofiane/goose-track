6b6b6a5cee5d119182a9038e27213b9474d309c0
chadjaa-sofiane <sofianne012@gmail.com>
2023-10-31 16:55:48 +0100

    add new color accents.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ export default {
         extend: {
             colors: {
                 accents: {
-                    1: 'hsl(var(--color-accent-100) / <alpha-value>)',
-                    2: 'hsl(var(--color-accent-200) / <alpha-value>)',
-                    3: 'hsl(var(--color-accent-2) / <alpha-value>)',
+                    1: 'hsl(var(--color-accent-1) / <alpha-value>)',
+                    2: 'hsl(var(--color-accent-2) / <alpha-value>)',
+                    3: 'hsl(var(--color-accent-3) / <alpha-value>)',
+                    4: 'hsl(var(--color-accent-4) / <alpha-value>)',
+                    5: 'hsl(var(--color-accent-5) / <alpha-value>)',
+                    6: 'hsl(var(--color-accent-6) / <alpha-value>)',
                 },
                 bg: 'hsl(var(--color-bg) / <alpha-value>)',
                 text: 'hsl(var(--color-text) / <alpha-value>)',
