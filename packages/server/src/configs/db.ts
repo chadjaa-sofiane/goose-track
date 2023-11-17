78b3ac34a5fc675aff663d56c8461007fda8676e
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-17 17:29:07 +0100

    configure mongoose connection.

---
+++ packages/server/src/configs/db.ts
@@ -0,0 +1,40 @@
+import mongoose, { ConnectOptions } from 'mongoose'
+import config from './config'
+import chalk from 'chalk'
+
+const options: ConnectOptions = {}
+
+const createMongoose = () => {
+    const uri = config.database.url
+    return {
+        connect: async () => {
+            try {
+                await mongoose.connect(uri, options)
+                console.log(
+                    chalk.greenBright(`
+                        connected to the database succesfuly! 
+                        database name: ${chalk.white(
+                            mongoose.connection.db.databaseName
+                        )}
+                            `)
+                )
+            } catch (error) {
+                console.error(error)
+            }
+        },
+        disconnect: async () => {
+            try {
+                await mongoose.disconnect()
+                console.log(
+                    chalk.redBright(`
+                    disconnected from the database succesfuly! 
+                `)
+                )
+            } catch (error) {
+                console.error(error)
+            }
+        },
+    }
+}
+
+export default createMongoose
