b7b2380e8a53a60e523466318c927c46e1bd20f6
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-18 22:31:36 +0100

    implement everything inside the src/index file.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,46 @@
-console.log("start something here")
+import express from 'express'
+import chalk from 'chalk'
+import config from '@/configs/config'
+import createMongoose from '@/configs/db'
+import authRouter from '@/routes/auth'
+import passport from 'passport'
+import jwtPassport from './configs/passport/jwt'
+
+export const mongooseConnection = createMongoose()
+config.env !== 'test' && (await mongooseConnection.connect())
+// await mongooseConnection.disconnect()
+
+export const app = express()
+
+// app middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// passport middlewares
+jwtPassport(passport)
+app.use(passport.initialize())
+
+// the routers
+app.use('/auth', authRouter)
+
+app.get(
+    '/protected-route',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        res.json({ message: 'Access granted' })
+    }
+)
+
+config.env !== 'test' &&
+    app.listen(config.port, () => {
+        console.log(
+            chalk.blueBright(
+                `the server is working on PORT: ${config.hostname}:${config.port}`
+            )
+        )
+    })
+
+process.on('uncaughtException', (error) => {
+    console.error(chalk.red('uncaught exception:', error))
+    process.exit(1)
+})
