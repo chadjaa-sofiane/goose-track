cdb7fb281afdb325359d81f678a2aae1e7cc8489
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-24 18:44:57 +0100

    add the validation and call the register function.

--- packages/client/src/features/auth/signupForm.tsx
+++ packages/client/src/features/auth/signupForm.tsx
@@ -1,33 +1,69 @@
 import { InputField } from "@/components/inputField"
-import AuthLayout, { AuthForm } from "./authLayout"
+import AuthLayout, { AuthForm, Spinner } from "./authUi"
 import { Button } from "@/components/button"
 import DoorIcon from "@/assets/exit.svg?react"
 import RegisterWelcomeSrc from "./assets/welcome-register.png"
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { RegisterFields, registerSchema, register as signUp } from "@/api/authApi"
 
 const SignUpForm = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors, isSubmitting, touchedFields }, setError } = useForm<RegisterFields>({
+        resolver: zodResolver(registerSchema),
+        mode: "onBlur"
+    })
 
-    const onSubmit = (data: unknown) => {
-        console.log(data);
+    const onSubmit = async (data: RegisterFields) => {
+        const result = await signUp({
+            email: data.email,
+            password: data.password,
+            name: data.name
+        })
+        if (!result) return
+        if (result.errors) {
+            for (const error in result.errors) {
+                setError(error as keyof RegisterFields, {
+                    type: "manual",
+                    message: result.errors[error as keyof RegisterFields]
+                })
+            }
+            return
+        }
+        alert("you registered successfully")
     }
-
     return <AuthLayout>
         <div className="flex flex-col gap-y-6 items-center">
             <AuthForm onSubmit={handleSubmit(onSubmit)}>
                 <h1 className="text-accents-1 text-2xl"> Sign up </h1>
                 <div className="flex flex-col gap-y-[1.25em]">
-                    <InputField label="name" {...register("name")} />
-                    <InputField label="email" {...register("email")} />
-                    <InputField label="password" {...register("password")} />
+                    <InputField
+                        label="name"
+                        status={errors["name"] ? "error" : (touchedFields["name"] ? "done" : "normal")}
+                        message={errors["name"] ? errors["name"]?.message : ''}
+                        {...register("name")}
+                    />
+                    <InputField
+                        label="email"
+                        status={errors["email"] ? "error" : (touchedFields["email"] ? "done" : "normal")}
+                        message={errors["email"] ? errors["email"]?.message : ""}
+                        {...register("email")}
+                    />
+                    <InputField
+                        label="password"
+                        status={errors["password"] ? "error" : (touchedFields["password"] ? "done" : "normal")}
+                        message={errors["password"] ? errors["password"]?.message : ""}
+                        {...register("password")}
+                        type="password"
+                    />
                 </div>
                 <Button
-                    className="flex justify-center"
+                    className="flex justify-center disabled:cursor-not-allowed disabled:bg-opacity-10 disabled:text-opacity-10"
                     type="submit"
+                    disabled={isSubmitting}
                     icons={{
-                        end: <DoorIcon className="stroke-white" />
-                    }}>Log in</Button>
+                        end: isSubmitting ? <Spinner /> : <DoorIcon className="stroke-white" />
+                    }}>{isSubmitting ? "loading..." : "Sign up"}</Button>
             </AuthForm>
             <div className="absolute hidden left-[5%] bottom-0 xl:left-[15%] xl:grid place-items-center -z-10">
                 <img src={RegisterWelcomeSrc} alt="Quickly come in and write down your tasks for the day!" />
@@ -36,6 +72,7 @@ const SignUpForm = () => {
                 <span className="text-accents-1 underline text-[1.25rem]">Log in</span>
             </Link>
         </div>
+
     </AuthLayout >
 }
 
