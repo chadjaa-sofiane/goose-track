a79a6d2cbf81e0ce32bd5b1a0bd95431725214ec
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-24 18:42:22 +0100

    create the extraction errors functions.

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { twMerge } from 'tailwind-merge'
 import clsx, { ClassValue } from 'clsx'
 import dayjs from 'dayjs'
+import type { ZodIssue } from 'zod'
 
 export const cn = (...inputs: ClassValue[]) => {
     return twMerge(clsx(inputs))
@@ -22,4 +23,47 @@ export const getDaysOfMonth = (
     }
 
     return daysOfMonth
+}
+
+export const extractErrorsFromIssues = (issues: ZodIssue[] = []) => {
+    return issues.reduce((acc, issue) => {
+        acc[issue.path[1]] = issue.message
+        return acc
+    }, {} as Record<string, string>)
+}
+
+/*
+
+{
+  "error": {
+    "index": 0,
+    "code": 11000,
+    "keyPattern": {
+      "email": 1
+    },
+    "keyValue": {
+      "email": "chadjaa@gmail.com"
+    }
+  },
+  "data": null
+}
+
+*/
+type MongooseError = {
+    name: string
+    message: string
+    code: number
+    keyPattern: Record<string, number>
+    keyValue: Record<string, string>
+    index: number
+}
+
+export const extractMongooseErrors = (error: MongooseError | null) => {
+    if (!error) return {}
+    if (error?.code === 11000) {
+        const keys = Object.keys(error.keyPattern)
+        return {
+            [keys[0]]: `${keys[0]} already exists !`,
+        }
+    }
 }
