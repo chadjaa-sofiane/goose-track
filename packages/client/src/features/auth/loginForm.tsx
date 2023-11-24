4cdbcb4a92d6493c57f6306a2d119ce691a0a33b
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-24 18:44:48 +0100

    add the validation and call the login function.

--- packages/client/src/features/auth/loginForm.tsx
+++ packages/client/src/features/auth/loginForm.tsx
@@ -1,21 +1,60 @@
 import { InputField } from "@/components/inputField"
-import AuthLayout, { AuthForm } from "./authLayout"
-import { useState } from "react"
+import AuthLayout, { AuthForm } from "./authUi"
 import { Button } from "@/components/button"
 import DoorIcon from "@/assets/exit.svg?react"
 import LoginWelcomeSrc from "./assets/welcome-login.png"
 import { Link } from "react-router-dom"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { LoginFields, login, loginSchema } from "@/api/authApi"
 
 const LoginForm = () => {
-    const [value, setValue] = useState("")
+    const { register, handleSubmit, formState: { errors, touchedFields }, setError } = useForm<LoginFields>({
+        resolver: zodResolver(loginSchema)
+    })
 
+    const onSubmit = async (data: LoginFields) => {
+        const result = await login({
+            email: data.email,
+            password: data.password
+        })
+        if (!result) {
+            return
+        }
+        if (result.errors) {
+            for (const error in result.errors) {
+                setError(error as keyof LoginFields, {
+                    type: "manual",
+                    message: result.errors[error as keyof LoginFields]
+                })
+            }
+            return
+        }
+        alert("you logged in successfully")
+    }
+
+    const showEmailErorr = errors["email"] && touchedFields["email"]
+    const showPasswordErorr = errors["password"] && touchedFields["password"]
     return <AuthLayout>
         <div className="flex flex-col gap-y-6 items-center">
-            <AuthForm>
+            <AuthForm onSubmit={handleSubmit(onSubmit)}>
                 <h1 className="text-accents-1 text-2xl"> Log In </h1>
                 <div className="flex flex-col gap-y-[1.25em]">
-                    <InputField name="email" label="email" value={value} onChange={(e) => setValue(e.target.value)} placeHolder="" />
-                    <InputField name="password" label="password" value={value} type="password" onChange={(e) => setValue(e.target.value)} />
+                    <InputField
+                        label="email"
+                        status={showEmailErorr ? "error" : (touchedFields["email"] ? "done" : "normal")}
+                        message={showEmailErorr ? errors["email"]?.message : ""}
+                        {...register("email")}
+                    />
+                    <InputField
+                        // fill the rest here 
+                        label="password"
+                        className=""
+                        status={showPasswordErorr ? "error" : (touchedFields["password"] ? "done" : "normal")}
+                        message={showPasswordErorr ? errors["password"]?.message : ""}
+                        {...register("password")}
+                        type="password"
+                    />
                 </div>
                 <Button
                     className="flex justify-center"
