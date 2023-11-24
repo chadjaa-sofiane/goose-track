163a01ac93b6645fd03dd3aa3379cb92c2e62e2a
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-24 18:43:53 +0100

    create the login and regester request api.

---
+++ packages/client/src/api/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+// import type { AxiosResponse, AxiosError } from 'axios'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+
+console.log('this is the base URL', BASE_URL)
+
+export const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+// Add an interceptor for global error handling
+// api.interceptors.response.use(
+//     (response: AxiosResponse) => response,
+//     (error: AxiosError) => {
+//         // Handle error globally
+//         if (error.code === 'ECONNREFUSED') {
+//             console.error('Network error:', error)
+//             return Promise.reject(error)
+//         }
+//         if (error.status === 400) {
+//             console.error('API request error:', error)
+//             return Promise.reject(error)
+//         }
+//         if (error.status === 500) {
+//             console.error('API request error:', error)
+//             return Promise.reject(error)
+//         }
+//     }
+// )
