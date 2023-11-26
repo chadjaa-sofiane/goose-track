e360ffea1e9c406221a9d6cc48941a98258c0331
chadjaa-sofiane <sofianne012@gmail.com>
2023-11-26 15:55:50 +0100

    add error style to the form wrapper.

--- packages/client/src/features/auth/authUi.tsx
+++ packages/client/src/features/auth/authUi.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils"
 
-
 interface AuthLayoutProps {
     children: React.ReactNode
 }
@@ -11,10 +10,15 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
     </div>
 }
 
+interface AuthFormProps extends React.ComponentPropsWithoutRef<"form"> {
+    error?: boolean
+}
 
-export const AuthForm = ({ children, className, ...rest }: React.ComponentPropsWithoutRef<"form">) => {
+export const AuthForm = ({ children, className, error = false, ...rest }: AuthFormProps) => {
     return (
-        <form {...rest} className={cn("w-[30em] max-w-full p-10 border border-accents-1 flex flex-col gap-y-10 bg-bg rounded-lg border-opacity-25 dark:bg-accents-1 dark:bg-opacity-5", className)}>
+        <form {...rest} className={cn("w-[30em] max-w-full p-10 border border-accents-1 flex flex-col gap-y-10 bg-bg rounded-lg border-opacity-25 dark:bg-accents-1 dark:bg-opacity-5", {
+            "border-error": error
+        }, className)}>
             {children}
         </form>
     )
