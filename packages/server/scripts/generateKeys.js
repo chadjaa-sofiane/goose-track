d7a2d866017c920b14fde5b63f88213cb64031af
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-01 15:37:36 +0100

    upload scripts files.

--- packages/server/scripts/generateKeys.js
+++ packages/server/scripts/generateKeys.js
@@ -1,5 +1,10 @@
 import crypto from 'crypto'
 import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const keypairs = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
@@ -16,5 +21,11 @@ const keypairs = crypto.generateKeyPairSync('rsa', {
 const publicKey = keypairs.publicKey
 const privateKey = keypairs.privateKey
 
-fs.writeFileSync('./public-key.pem', publicKey)
-fs.writeFileSync('./private-key.pem', privateKey)
+const publicKeyPath = join(__dirname, '../public-key.pem')
+const privateKeyPath = join(__dirname, '../private-key.pem')
+
+fs.writeFileSync(publicKeyPath, publicKey)
+fs.writeFileSync(privateKeyPath, privateKey)
+
+console.log(`Writing public key to ${publicKey}`)
+console.log(`Writing private key to ${privateKey}`)
