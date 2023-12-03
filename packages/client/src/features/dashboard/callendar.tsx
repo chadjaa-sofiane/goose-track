9a26b91100d26ee31cb2752b7f43f28860ba0010
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-03 15:03:54 +0100

    create temporary dashboard pages to render.

---
+++ packages/client/src/features/dashboard/callendar.tsx
@@ -0,0 +1,9 @@
+import { DashboardPageLayout } from "./dashboardLayout"
+
+const Callendar = () => {
+    return <DashboardPageLayout title="Callendar">
+        something else should be in here
+    </DashboardPageLayout>
+}
+
+export default Callendar                                
