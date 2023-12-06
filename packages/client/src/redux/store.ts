aee89fc417226a62ac51e1a25cea51fa5151cf32
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-06 15:53:06 +0100

    add auth slice to the store.

--- packages/client/src/redux/store.ts
+++ packages/client/src/redux/store.ts
@@ -1,14 +1,15 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit'
 import userReduer from './userSlice'
+import authSlice from './authSlice'
 
 const store = configureStore({
     reducer: combineSlices({
+        auth: authSlice,
         user: userReduer,
     }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch;
-
+export type AppDispatch = typeof store.dispatch
 
 export default store
