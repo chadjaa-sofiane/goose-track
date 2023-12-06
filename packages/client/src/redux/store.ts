5c31aed3b1bab69d946f022c03d3fcfd032f12a2
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-06 19:32:11 +0100

    build createStore function.

--- packages/client/src/redux/store.ts
+++ packages/client/src/redux/store.ts
@@ -2,12 +2,15 @@ import { combineSlices, configureStore } from '@reduxjs/toolkit'
 import userReduer from './userSlice'
 import authSlice from './authSlice'
 
-const store = configureStore({
-    reducer: combineSlices({
-        auth: authSlice,
-        user: userReduer,
-    }),
-})
+export const createStore = () =>
+    configureStore({
+        reducer: combineSlices({
+            auth: authSlice,
+            user: userReduer,
+        }),
+    })
+
+const store = createStore()
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
