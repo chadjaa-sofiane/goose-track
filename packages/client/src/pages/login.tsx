43470133d9edfb64b442087069b3b2b19adfbba3
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-06 15:52:16 +0100

    remove the auth function from the login and signup.

--- packages/client/src/pages/login.tsx
+++ packages/client/src/pages/login.tsx
@@ -1,10 +1,9 @@
 import { LoginForm } from "@/features/auth"
 import ThemedLayout from "@/layouts/themedLayout"
-import { login } from "@/api/authApi"
 
 const Login = () => {
     return <ThemedLayout className="bg-[#dcebf7] dark:bg-bg">
-        <LoginForm login={login} />
+        <LoginForm />
     </ThemedLayout>
 }
 
--- packages/client/src/pages/signup.tsx
+++ packages/client/src/pages/signup.tsx
@@ -1,11 +1,9 @@
 import { SignupForm } from "@/features/auth"
 import ThemedLayout from "@/layouts/themedLayout"
-import { register } from "@/api/authApi"
-
 
 const Signup = () => {
     return <ThemedLayout className="bg-[#dcebf7] dark:bg-bg">
-        <SignupForm signUp={register} />
+        <SignupForm/>
     </ThemedLayout>
 }
 
