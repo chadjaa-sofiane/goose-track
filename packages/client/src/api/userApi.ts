599035e11a7b744f9f6c7e5332f24a683d911ec6
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-10 12:58:03 +0100

    build the post request for the user data update.

--- packages/client/src/api/userApi.ts
+++ packages/client/src/api/userApi.ts
@@ -1,7 +1,9 @@
 import { isAxiosError } from 'axios'
-import { api } from '.'
+import { Response, api } from '.'
+import { extractErrorsFromIssues } from '@/lib/utils'
+import { ZodIssue, z } from 'zod'
 
-export interface UserData {
+export interface UserFields {
     name: string
     email: string
     phone?: string
@@ -9,19 +11,21 @@ export interface UserData {
     skype?: string
 }
 
-export type GetUserDataResult = {
-    success: boolean
-    data: UserData | null
-    errors: null
-}
+export type ReturnedUserData = {
+    _id: string
+    createdAt: string
+    updatedAt: string
+} & UserFields
 
-export const getUserData = async (): Promise<GetUserDataResult> => {
+export type GetUserDataResponse = Response<ReturnedUserData, UserFields>
+
+export const getUserData = async (): Promise<GetUserDataResponse> => {
     try {
-        const result = await api.get('/user/me')
+        const result = await api.get<{ user: ReturnedUserData }>('/user/me')
 
         return {
             success: true,
-            data: result.data,
+            data: result.data.user,
             errors: null,
         }
     } catch (error) {
@@ -39,5 +43,87 @@ export const getUserData = async (): Promise<GetUserDataResult> => {
             data: null,
             errors: null,
         }
+    }
+}
+
+const stringPreProcess = (name: unknown) => {
+    if (!name || typeof name !== 'string') return undefined
+    return name.trim() === '' ? undefined : name
+}
+
+export const updateUserSchema = z.object({
+    name: z.preprocess(
+        stringPreProcess,
+        z.string().min(3, 'Name must be at least 3 characters long').optional()
+    ),
+    email: z.preprocess(
+        stringPreProcess,
+        z.string().email('Invalid email format').optional()
+    ),
+    phone: z.preprocess(
+        stringPreProcess,
+        z
+            .string()
+            .regex(
+                /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
+                'please provide a valid phone number'
+            )
+            .optional()
+    ),
+    birthday: z.preprocess(
+        stringPreProcess,
+        z.coerce
+            .date()
+            .transform((date) => date.toISOString())
+            .optional()
+            .nullable()
+    ),
+    skype: z.preprocess(
+        stringPreProcess,
+        z
+            .string()
+            .min(3, 'Skype username must be at least 3 characters long')
+            .optional()
+    ),
+})
+
+export type UpdateUserFields = z.infer<typeof updateUserSchema>
+
+export type UpdateUserDataInputs = Partial<UpdateUserFields>
+export type UpdateUserDataResponse = Response<
+    ReturnedUserData,
+    UpdateUserFields
+>
+
+export const updateUserData = async (
+    inputs?: UpdateUserDataInputs
+): Promise<UpdateUserDataResponse> => {
+    try {
+        const result = await api.put<{ data: ReturnedUserData }>(
+            'user/update',
+            inputs
+        )
+
+        return {
+            success: true,
+            data: result.data.data,
+            errors: null,
+        }
+    } catch (error) {
+        if (isAxiosError(error)) {
+            const data = error.response?.data
+            return {
+                success: false,
+                data: null,
+                errors: {
+                    ...extractErrorsFromIssues(data?.issues as ZodIssue[]),
+                } as UpdateUserFields,
+            }
+        }
+    }
+    return {
+        success: false,
+        data: null,
+        errors: null,
     }
 }
