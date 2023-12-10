b55c5252434665863d694a93d02aa12d80d2ebec
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-10 13:03:21 +0100

    change the style for the dashboard layout.

--- packages/client/src/features/dashboard/dashboardLayout.tsx
+++ packages/client/src/features/dashboard/dashboardLayout.tsx
@@ -1,27 +1,36 @@
-import ThemedLayout from "@/layouts/themedLayout"
-import { NavigationBar } from "./components/navigationBar"
-import React, { useState } from "react"
-import { Outlet, useOutletContext } from "react-router-dom"
-import { Header } from "./components/header"
-import DashboardRoutesfrom from "@/features/dashboard/routes"
+import ThemedLayout from '@/layouts/themedLayout'
+import { NavigationBar } from './components/navigationBar'
+import React, { useState } from 'react'
+import { Outlet, useOutletContext } from 'react-router-dom'
+import { Header } from './components/header'
+import DashboardRoutesfrom from '@/features/dashboard/routes'
 
 export type OutletContextType = {
-    openNav: boolean;
-    setOpenNav: React.Dispatch<React.SetStateAction<boolean>>;
+    openNav: boolean
+    setOpenNav: React.Dispatch<React.SetStateAction<boolean>>
 }
-
 
 const DashboardLayout = () => {
     const [openNav, setOpenNav] = useState(false)
 
-    return <ThemedLayout className="min-h-screen bg-[#dcebf7] dark:bg-bg">
-        <main className="h-screen flex items-stretch">
-            <NavigationBar open={openNav} setOpen={setOpenNav} routes={DashboardRoutesfrom} />
-            <div className="flex-1 flex flex-col gap-y-[2.375em] py-8 px-[2.875em] dark:bg-[#171820] ">
-                <Outlet context={{ openNav, setOpenNav } satisfies OutletContextType} />
-            </div>
-        </main>
-    </ThemedLayout>
+    return (
+        <ThemedLayout className="min-h-screen bg-[#dcebf7] dark:bg-bg">
+            <main className="flex items-stretch">
+                <NavigationBar
+                    open={openNav}
+                    setOpen={setOpenNav}
+                    routes={DashboardRoutesfrom}
+                />
+                <div className="w-full min-h-screen h-fit flex flex-col gap-y-[2.375em] px-5 py-6 md:py-8 md:px-[2.875em] dark:bg-[#171820]">
+                    <Outlet
+                        context={
+                            { openNav, setOpenNav } satisfies OutletContextType
+                        }
+                    />
+                </div>
+            </main>
+        </ThemedLayout>
+    )
 }
 
 interface DashboardPageLayoutProps {
@@ -29,14 +38,19 @@ interface DashboardPageLayoutProps {
     children: React.ReactNode
 }
 
-export const DashboardPageLayout = ({ title, children }: DashboardPageLayoutProps) => {
+export const DashboardPageLayout = ({
+    title,
+    children,
+}: DashboardPageLayoutProps) => {
     const { setOpenNav } = useOutletContext<OutletContextType>()
-    return <>
-        <Header title={title} setOpen={setOpenNav} />
-        <section>
-            {children}
-        </section>
-    </>
+    return (
+        <>
+            <Header title={title} setOpen={setOpenNav} />
+            <section className="flex flex-col justify-center">
+                {children}
+            </section>
+        </>
+    )
 }
 
 export default DashboardLayout
