91adc83d1a709e1650f3b497e4dc0d3f3dee2d29
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-10 13:05:30 +0100

    add user update in the user slice.

--- packages/client/src/redux/userSlice.tsx
+++ packages/client/src/redux/userSlice.tsx
@@ -1,50 +1,71 @@
-import { getUserData, UserData } from "@/api/userApi"
-import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit"
-import { setIsLoggedIn } from "./authSlice"
-
+import {
+    getUserData,
+    updateUserData,
+    UpdateUserDataInputs,
+    UserFields,
+} from '@/api/userApi'
+import {
+    createAsyncThunk,
+    createSlice,
+    type PayloadAction,
+} from '@reduxjs/toolkit'
+import { setIsLoggedIn } from './authSlice'
 
 type UserStates = {
     isLoading: boolean
-    data: UserData | null
+    data: UserFields | null
     errors: unknown
 }
 
 const initialState: UserStates = {
     isLoading: true,
     data: null,
-    errors: null
+    errors: null,
 }
 
-
-export const getUserDataAsync = createAsyncThunk("user/getUserData", async (_, { dispatch }) => {
-    dispatch(setIsLoading(true));
-    const result = await getUserData();
-    if (result.success) {
-        dispatch(setIsLoggedIn(true))
+export const getUserDataAsync = createAsyncThunk(
+    'user/getUserData',
+    async (_, { dispatch }) => {
+        dispatch(setIsLoading(true))
+        const result = await getUserData()
+        if (result.success) {
+            dispatch(setIsLoggedIn(true))
+        }
+        dispatch(setIsLoading(false))
+        return result
     }
-    dispatch(setIsLoading(false))
-    return result
-})
+)
+
+export const updateUserDataAsync = createAsyncThunk(
+    'user/updateUserData',
+    async (credentials?: UpdateUserDataInputs) => {
+        return await updateUserData(credentials)
+    }
+)
 
 const userSlice = createSlice({
-    name: "user",
+    name: 'user',
     initialState,
     reducers: {
         setIsLoading: (state, action: PayloadAction<boolean>) => {
-            state.isLoading = action.payload;
+            state.isLoading = action.payload
         },
-        setUserData: (state, action: PayloadAction<UserData | null>) => {
+        setUserData: (state, action: PayloadAction<UserFields>) => {
             state.data = action.payload
-        }
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(getUserDataAsync.fulfilled, (state, action) => {
             state.data = action.payload.data
             state.errors = action.payload.errors
         })
-    }
+        builder.addCase(updateUserDataAsync.fulfilled, (state, action) => {
+            if (action.payload?.success) {
+                state.data = action.payload.data
+            }
+        })
+    },
 })
-
 
 export const { setIsLoading } = userSlice.actions
 export default userSlice.reducer
