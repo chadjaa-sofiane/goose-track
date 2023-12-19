28fbcf211febcbc1ea8cbd113340001c373284be
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-19 14:57:01 +0100

    add data for testing.

---
+++ packages/client/src/redux/data/tasks.ts
@@ -0,0 +1,76 @@
+import { Task } from '../calendarSlice'
+
+// To-Do List
+export const toDoList: Task[] = [
+    {
+        id: '1',
+        userId: 'YourUserID',
+        title: 'Embark on the Journey of Knowledge',
+        start: 'Now',
+        end: 'When wisdom is gained',
+    },
+    {
+        id: '2',
+        userId: 'YourUserID',
+        title: 'Conquer the Mountain of Challenges',
+        start: 'Dawn',
+        end: 'Dusk',
+    },
+    {
+        id: '3',
+        userId: 'YourUserID',
+        title: 'Cultivate the Garden of Relationships',
+        start: 'Today',
+        end: 'Endless',
+    },
+]
+
+// In Progress List
+export const inProgressList: Task[] = [
+    {
+        id: '4',
+        userId: 'YourUserID',
+        title: 'Building the Bridge of Understanding',
+        start: 'Yesterday',
+        end: 'Tomorrow',
+    },
+    {
+        id: '5',
+        userId: 'YourUserID',
+        title: 'Crafting the Magnum Opus of Creativity',
+        start: 'Last week',
+        end: 'Future unknown',
+    },
+    {
+        id: '6',
+        userId: 'YourUserID',
+        title: 'Nurturing the Flame of Resilience',
+        start: 'This month',
+        end: 'To be determined',
+    },
+]
+
+// Done List
+export const doneList: Task[] = [
+    {
+        id: '7',
+        userId: 'YourUserID',
+        title: 'Master the Art of Time Management',
+        start: 'Sometime past',
+        end: 'Now',
+    },
+    {
+        id: '8',
+        userId: 'YourUserID',
+        title: 'Explore the Uncharted Territory of Knowledge',
+        start: 'Previously',
+        end: 'Present',
+    },
+    {
+        id: '9',
+        userId: 'YourUserID',
+        title: 'Cultivate the Habit of Gratitude',
+        start: 'Earlier',
+        end: 'Now',
+    },
+]
