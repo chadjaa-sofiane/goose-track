08d82a1e22aaeb178b481b4f5a2a8b7b61b40a09
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-23 18:21:29 +0100

    create the calendar Api file.

---
+++ packages/client/src/api/calendarApi.ts
@@ -0,0 +1,36 @@
+import { parseTime } from '@/lib/utils'
+import { priorites } from '@/redux/calendarSlice'
+import { z } from 'zod'
+
+const timeRegex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/
+
+const validTimeRange = (start: string, end: string) => {
+    const parsedStart = parseTime(start) // Use a time parsing library
+    const parsedEnd = parseTime(end)
+    return parsedEnd > parsedStart
+}
+
+const validTime = (value: string) => {
+    if (!timeRegex.test(value)) {
+        throw new Error('Invalid time format.')
+    }
+    return true
+}
+
+export const addTaskschema = z
+    .object({
+        title: z
+            .string()
+            .min(5, 'the title should contain at least 5 letters')
+            .max(100, 'this title is to long'),
+        start: z.string().refine(validTime),
+        end: z.string().refine(validTime),
+    })
+    .refine((data) => validTimeRange(data.start, data.end), {
+        message: 'End time cannot be before start time',
+        path: ['start'],
+    })
+
+export type addTaskFields = z.infer<typeof addTaskschema> & {
+    priority: (typeof priorites)[number]
+}
