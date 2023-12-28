7f983fbb7df2b19ff2b020c805515a95b92561cf
chadjaa-sofiane <sofianne012@gmail.com>
2023-12-28 23:13:30 +0100

    implement the edit task and delete task in the day page.

--- packages/client/src/features/dashboard/calendar/components/calendarBody/calendarDay.tsx
+++ packages/client/src/features/dashboard/calendar/components/calendarBody/calendarDay.tsx
@@ -4,14 +4,13 @@ import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
 import { cn, debounce, getDaysOfWeek } from '@/lib/utils'
 import {
     WEEK_DAYS,
-    markTaskAsDone,
-    markTaskAsInProgress,
-    markTaskAsToDo,
+    createDayTask,
+    markTask,
+    setDate,
 } from '@/redux/calendarSlice'
 import PlusIcon from '@/assets/plus.svg?react'
-import { Button } from '@/components/button'
 import { motion } from 'framer-motion'
-import dayjs from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 import {
     DndContext,
     useDraggable,
@@ -19,8 +18,15 @@ import {
     DragEndEvent,
     useDroppable,
     DragOverlay,
+    DraggableAttributes,
 } from '@dnd-kit/core'
 import { Task } from '@/redux/calendarSlice'
+import { AddTask, UseEditTask } from './TaskForm'
+import CircleArrowIcon from '@/features/dashboard/assets/circle-arrow.svg?react'
+import TrashIcon from '@/features/dashboard/assets/trash.svg?react'
+import PencilIcon from '@/features/dashboard/assets/pencil.svg?react'
+import { type SyntheticListenerMap } from '@dnd-kit/core/dist/hooks/utilities'
+import { UseDeleteTask } from './deleteTask'
 
 const CalendarDay = () => {
     return (
@@ -36,18 +42,35 @@ const WeekDaysField = () => {
     const month = useAppSelector((state) => state.calendar.month)
     const year = useAppSelector((state) => state.calendar.year)
 
+    const dispatch = useAppDispatch()
+
+    const setDateHandler = (weekDate: Dayjs) => {
+        const year = weekDate.year()
+        const month = weekDate.month()
+        const date = weekDate.date()
+        dispatch(setDate({ year, month, date }))
+    }
+
     return (
-        <div className="sticky grid grid-cols-7 bg-accents-6 py-3.5 rounded-lg border-2 border-[#42434b]">
+        <div className="sticky grid grid-cols-7 bg-accents-6  rounded-lg border-2 border-[#42434b]">
             {getDaysOfWeek(year, month, date).map((weekDate, index) => (
                 <div
-                    className={cn('flex flex-col items-center', {
-                        'text-accents-1 ': dayjs().isSame(weekDate, 'date'),
-                    })}
+                    className={cn(
+                        'flex flex-col items-center cursor-pointer rounded-lg py-3.5',
+                        {
+                            'text-accents-1 ': dayjs().isSame(weekDate, 'date'),
+                            'bg-bg/50  border border-accents-1':
+                                weekDate.isSame(
+                                    dayjs().year(year).month(month).date(date),
+                                    'date'
+                                ),
+                        }
+                    )}
                     key={index}
+                    onPointerDown={() => setDateHandler(weekDate)}
                 >
                     <span className="text-[#62636a] first-letter:text-base text-[0px] md:text-base text-center">
-                        {' '}
-                        {WEEK_DAYS[index]}{' '}
+                        {WEEK_DAYS[index]}
                     </span>
                     <span> {weekDate.date()} </span>
                 </div>
@@ -56,14 +79,27 @@ const WeekDaysField = () => {
     )
 }
 
-type TaskContainerId = 'todo' | 'progress' | 'done'
+export type TaskContainerId = 'todo' | 'progress' | 'done'
 
 const TasksSpaceContainer = () => {
     const [activeId, setActiveId] = useState<string | null>(null)
+    const date = useAppSelector((state) => state.calendar.date)
+    const month = useAppSelector((state) => state.calendar.month)
+    const year = useAppSelector((state) => state.calendar.year)
+
+    const tasksDate = `${year}-${month}-${date}`
 
     const dispatch = useAppDispatch()
     const [width, setWidth] = useState(0)
     const ref = useRef<HTMLDivElement>(null)
+
+    const tasks = useAppSelector((state) => state.calendar.tasks[tasksDate])
+
+    useEffect(() => {
+        if (!tasks) {
+            dispatch(createDayTask({ date: tasksDate }))
+        }
+    }, [tasks, tasksDate, dispatch])
 
     useEffect(() => {
         const updateWidth = debounce(() => {
@@ -84,30 +120,20 @@ const TasksSpaceContainer = () => {
         setActiveId(id)
     }
 
-    const markAsInProgressHandler = (id: string) => {
-        dispatch(markTaskAsInProgress(id))
-    }
-    const markAsDoneHandler = (id: string) => {
-        dispatch(markTaskAsDone(id))
-    }
-    const markAsToDoHandler = (id: string) => {
-        dispatch(markTaskAsToDo(id))
-    }
-
     const handleDragEnd = (event: DragEndEvent) => {
         const id = event.active.id as string
-        const containerId = event?.over?.id as TaskContainerId | null
+        const targetContainer = event?.over?.id as string
+        const sourceContainer = event?.active?.data?.current
+            ?.container as string
 
-        switch (containerId) {
-            case 'done':
-                markAsDoneHandler(id)
-                break
-            case 'progress':
-                markAsInProgressHandler(id)
-                break
-            case 'todo':
-                markAsToDoHandler(id)
-                break
+        if (targetContainer && sourceContainer !== targetContainer) {
+            dispatch(
+                markTask({
+                    container: targetContainer,
+                    date: tasksDate,
+                    id,
+                })
+            )
         }
 
         setActiveId(null)
@@ -125,9 +151,19 @@ const TasksSpaceContainer = () => {
                     onDragStart={handleDragStart}
                     onDragEnd={handleDragEnd}
                 >
-                    <TodoContainer activeId={activeId} />
-                    <ProgressContainer activeId={activeId} />
-                    <DoneContainer activeId={activeId} />
+                    {tasks?.containers &&
+                        Object.entries(tasks?.containers)?.map(
+                            ([name, container]) => (
+                                <TasksContainer
+                                    key={name}
+                                    id={name}
+                                    activeId={activeId}
+                                    title={container.title}
+                                    tasks={container.tasks}
+                                    tasksDate={tasksDate}
+                                />
+                            )
+                        )}
                 </DndContext>
             </motion.div>
         </div>
@@ -138,50 +174,8 @@ interface TasksContainerProps {
     title: string
     tasks: Task[]
     activeId: string | null
-    id: TaskContainerId
-}
-
-const TodoContainer = ({ activeId }: Pick<TasksContainerProps, 'activeId'>) => {
-    const todoList = useAppSelector((state) => state.calendar.toDoList)
-
-    return (
-        <TasksContainer
-            id="todo"
-            activeId={activeId}
-            title="To Do"
-            tasks={todoList}
-        />
-    )
-}
-
-const ProgressContainer = ({
-    activeId,
-}: Pick<TasksContainerProps, 'activeId'>) => {
-    const inProgressList = useAppSelector(
-        (state) => state.calendar.inProgressList
-    )
-
-    return (
-        <TasksContainer
-            id="progress"
-            activeId={activeId}
-            title="In Progress"
-            tasks={inProgressList}
-        />
-    )
-}
-
-const DoneContainer = ({ activeId }: Pick<TasksContainerProps, 'activeId'>) => {
-    const doneList = useAppSelector((state) => state.calendar.doneList)
-
-    return (
-        <TasksContainer
-            id="done"
-            activeId={activeId}
-            title="Done"
-            tasks={doneList}
-        />
-    )
+    id: string
+    tasksDate: string
 }
 
 const TasksContainer = ({
@@ -189,12 +183,22 @@ const TasksContainer = ({
     title,
     tasks,
     activeId,
+    tasksDate,
 }: TasksContainerProps) => {
     const portableRef = useRef<HTMLDivElement>(null)
     const { setNodeRef, isOver, active } = useDroppable({ id })
 
-    const sourceContainerId = active?.data?.current?.containerId as string
+    const sourceContainerId = active?.data?.current?.container as string
 
+    const { EditTask, openEditTask } = UseEditTask({
+        container: id,
+        date: tasksDate,
+        title,
+    })
+    const { DeleteTask, openDeleteTask } = UseDeleteTask({
+        container: id,
+        date: tasksDate,
+    })
     return (
         <div className="w-[25em] shrink-0 flex flex-col gap-y-7 bg-accents-6 px-5 py-[1.125em] border border-[#42434b] rounded-lg ">
             <div className="flex justify-between">
@@ -217,87 +221,145 @@ const TasksContainer = ({
                     <Draggable
                         key={task.id}
                         id={task.id}
-                        containerId={id}
                         activeId={activeId}
                         overlayElement={portableRef.current}
-                    >
-                        <Task task={task} />
-                    </Draggable>
+                        task={task}
+                        container={id}
+                        openEditTask={openEditTask}
+                        openDeleteTask={openDeleteTask}
+                    />
                 ))}
                 {sourceContainerId !== id && isOver && activeId && (
                     <ListPreview />
                 )}
             </ul>
+            <EditTask />
+            <DeleteTask />
             <div className="w-full mt-auto">
-                <Button className="w-full grid place-items-center">
-                    Add Task
-                </Button>
+                <AddTask container={id} date={tasksDate} title={title} />
             </div>
         </div>
     )
 }
 
 interface DraggableProps {
-    children: React.ReactNode
     id: string
-    containerId: string
     activeId?: string | null
     overlayElement?: HTMLDivElement | null
+    container: string
+    task: Task
+    openEditTask: (task: Task) => void
+    openDeleteTask: (taskId: string) => void
 }
-
 const Draggable = ({
-    children,
     id,
-    containerId,
     activeId,
     overlayElement,
+    container,
+    task,
+    openEditTask,
+    openDeleteTask,
 }: DraggableProps) => {
-    const { setNodeRef, listeners, attributes, isDragging } = useDraggable({
+    const { setNodeRef, isDragging, listeners, attributes } = useDraggable({
         id,
-        data: { containerId },
+        data: {
+            container,
+        },
     })
 
     return (
-        <li
-            ref={setNodeRef}
-            {...listeners}
-            {...attributes}
-            className={cn({ 'opacity-60': isDragging })}
-            onPointerDownCapture={(e) => {
-                e.stopPropagation()
-                if (listeners) {
-                    listeners['onPointerDown'](e)
-                }
-            }}
-        >
+        <li ref={setNodeRef} className={cn({ 'opacity-60': isDragging })}>
             {overlayElement &&
                 activeId === id &&
-                createPortal(children, overlayElement)}
-            {children}
+                createPortal(<Task task={task} />, overlayElement)}
+            <Task
+                task={task}
+                listeners={listeners}
+                attributes={attributes}
+                openEditTask={openEditTask}
+                openDeleteTask={openDeleteTask}
+            />
         </li>
     )
 }
 
 interface TaskProps {
     task: Task
+    listeners?: SyntheticListenerMap | undefined
+    attributes?: DraggableAttributes
+    openEditTask?: (task: Task) => void
+    openDeleteTask?: (taskId: string) => void
 }
 
-const Task = ({ task }: TaskProps) => {
+const Task = ({
+    task,
+    listeners,
+    attributes,
+    openEditTask,
+    openDeleteTask,
+}: TaskProps) => {
     return (
-        <div className="p-3.5 bg-bg border border-[#42434b] rounded-md">
-            <p> {task.title} </p>
-        </div>
+        <motion.div
+            // onHoverStart={{  }}
+            className="flex flex-col gap-y-8 p-3.5 bg-bg border border-[#42434b] rounded-md"
+        >
+            <div
+                {...listeners}
+                {...attributes}
+                onPointerDownCapture={(e) => {
+                    e.stopPropagation()
+                    console.log(listeners)
+
+                    if (listeners) {
+                        listeners['onPointerDown'](e)
+                        listeners['onKeyDown'](e)
+                    }
+                }}
+            >
+                <p> {task.title} </p>
+            </div>
+            <div className="flex justify-between items-center">
+                <div className="flex gap-x-2">
+                    <div className="w-8 h-8 bg-slate-600 rounded-full"></div>
+                    <div
+                        className={cn(
+                            'px-3 py-1 text-white rounded-md first-letter:uppercase',
+                            {
+                                'bg-priorities-low': task.priority === 'low',
+                                'bg-priorities-medium':
+                                    task.priority === 'medium',
+                                'bg-priorities-high': task.priority === 'high',
+                            }
+                        )}
+                    >
+                        {' '}
+                        {task.priority}{' '}
+                    </div>
+                </div>
+                <div className="flex gap-x-2.5 cursor-pointer">
+                    <CircleArrowIcon className="hover:scale-125" />
+                    <PencilIcon
+                        onClick={() => openEditTask?.(task)}
+                        className="hover:scale-125"
+                    />
+                    <TrashIcon
+                        onClick={() => openDeleteTask?.(task.id)}
+                        className="hover:scale-125"
+                    />
+                </div>
+            </div>
+        </motion.div>
     )
 }
 
 const ListPreview = () => {
     return (
-        <div className="w-full h-12 p-3.5 bg-accents-1 bg-opacity-5 border-2 border-accents-1 rounded-md" />
+        <div className="w-full h-28 p-3.5 bg-accents-1 bg-opacity-5 border-2 border-accents-1 rounded-md" />
     )
 }
 
 const PortableElement = forwardRef<HTMLDivElement>((_, ref) => {
-    return <div ref={ref} className="w-[25em] h-4"></div>
+    return <div ref={ref} className="w-[22em] h-4"></div>
 })
 
 export default CalendarDay
